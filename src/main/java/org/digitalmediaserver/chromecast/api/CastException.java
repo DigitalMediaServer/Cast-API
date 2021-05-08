4233eab860a8744c89f94abcc079905f77a0cff0
Nadahar <Nadahar@users.noreply.github.com>
2021-05-08 05:54:27 +0200

    Extended CastException with JavaDocs and:   - LoadCancelledCastException   - LoadFailedCastException   - InvalidCastException   - LaunchErrorCastException

--- src/main/java/org/digitalmediaserver/chromecast/api/CastException.java
+++ src/main/java/org/digitalmediaserver/chromecast/api/CastException.java
@@ -16,6 +16,8 @@
 package org.digitalmediaserver.chromecast.api;
 
 import java.io.IOException;
+import javax.annotation.Nullable;
+
 
 /**
  * Generic error, which may happen during interaction with a cast device.
@@ -25,11 +27,76 @@ public class CastException extends IOException {
 
 	private static final long serialVersionUID = 1L;
 
-	public CastException(String message) {
+	/**
+	 * Creates a new instance with the specified message.
+	 *
+	 * @param message the message to use.
+	 */
+	public CastException(@Nullable String message) {
 		super(message);
 	}
 
-	public CastException(String message, Throwable cause) {
+	/**
+	 * Creates a new instance with the specified message and cause.
+	 *
+	 * @param message the message to use.
+	 * @param cause the cause to use.
+	 */
+	public CastException(@Nullable String message, @Nullable Throwable cause) {
 		super(message, cause);
+	}
+
+	public static class LoadCancelledCastException extends CastException {
+
+		private static final long serialVersionUID = 1L;
+
+		@Nullable
+		private final Integer itemId;
+
+		public LoadCancelledCastException(String message, @Nullable Integer itemId) {
+			super(message);
+			this.itemId = itemId;
+		}
+
+		@Nullable
+		public Integer getItemId() {
+			return itemId;
+		}
+	}
+
+	public static class LoadFailedCastException extends CastException {
+
+		private static final long serialVersionUID = 1L;
+
+		public LoadFailedCastException(String message) {
+			super(message);
+		}
+	}
+
+	public static class InvalidCastException extends CastException {
+
+		private static final long serialVersionUID = 1L;
+
+		public InvalidCastException(String message) {
+			super(message);
+		}
+	}
+
+	/**
+	 * A specialized {@link CastException} used when a {@code LAUNCH_ERROR}
+	 * response is received.
+	 */
+	public static class LaunchErrorCastException extends CastException {
+
+		private static final long serialVersionUID = 1L;
+
+		/**
+		 * Creates a new instance using the specified message.
+		 *
+		 * @param message the message to use.
+		 */
+		public LaunchErrorCastException(String message) {
+			super(message);
+		}
 	}
 }
