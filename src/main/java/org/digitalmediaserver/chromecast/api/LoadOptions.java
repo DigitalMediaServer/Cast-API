d446e49541207ec521573355163a601286d3c22e
Nadahar <Nadahar@users.noreply.github.com>
2021-05-17 14:42:36 +0200

    Created new protocol objects:   - ContainerMetadata   - ExtendedMediaStatus   - Image   - LiveSeekableRange   - LoadOptions   - MediaVolume   - QueueData   - TextTrackStyle   - VideoInformation

---
+++ src/main/java/org/digitalmediaserver/chromecast/api/LoadOptions.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2021 Digital Media Server developers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.digitalmediaserver.chromecast.api;
+
+import java.util.Locale;
+import java.util.Objects;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.Immutable;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+/**
+ * Provides additional options for load requests.
+ *
+ * @author Nadahar
+ */
+@Immutable
+public class LoadOptions {
+
+	/** The content filtering mode to apply for which items to play */
+	@Nullable
+	@JsonProperty
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	protected final ContentFilteringMode contentFilteringMode;
+
+	/**
+	 * Creates a new instance using the specified parameters.
+	 *
+	 * @param contentFilteringMode the content filtering mode to apply for which
+	 *            items to play.
+	 */
+	public LoadOptions(
+		@JsonProperty("contentFilteringMode") @Nullable ContentFilteringMode contentFilteringMode
+	) {
+		this.contentFilteringMode = contentFilteringMode;
+	}
+
+	/**
+	 * @return The content filtering mode to apply for which items to play.
+	 */
+	@Nullable
+	public ContentFilteringMode getContentFilteringMode() {
+		return contentFilteringMode;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(contentFilteringMode);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof LoadOptions)) {
+			return false;
+		}
+		LoadOptions other = (LoadOptions) obj;
+		return Objects.equals(contentFilteringMode, other.contentFilteringMode);
+	}
+
+	@Override
+	public String toString() {
+		return new StringBuilder()
+			.append(getClass().getSimpleName())
+			.append(" [contentFilteringMode=").append(contentFilteringMode).append("]")
+			.toString();
+	}
+
+	/**
+	 * Provides content filtering mode.
+	 */
+	public enum ContentFilteringMode {
+
+		/** Do not play explicit content */
+		FILTER_EXPLICIT;
+
+		/**
+		 * Parses the specified string and returns the corresponding
+		 * {@link ContentFilteringMode}, or {@code null} if no match could be
+		 * found.
+		 *
+		 * @param contentFilteringMode the string to parse.
+		 * @return The resulting {@link ContentFilteringMode} or {@code null}.
+		 */
+		@Nullable
+		@JsonCreator
+		public static ContentFilteringMode typeOf(String contentFilteringMode) {
+			if (Util.isBlank(contentFilteringMode)) {
+				return null;
+			}
+			String typeString = contentFilteringMode.toUpperCase(Locale.ROOT);
+			for (ContentFilteringMode type : values()) {
+				if (typeString.equals(type.name())) {
+					return type;
+				}
+			}
+			return null;
+		}
+	}
+}
---
+++ src/main/java/org/digitalmediaserver/chromecast/api/MediaVolume.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2021 Digital Media Server developers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.digitalmediaserver.chromecast.api;
+
+import java.util.Objects;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.Immutable;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+/**
+ * Represents the volume of a media session stream.
+ *
+ * @author Nadahar
+ */
+@Immutable
+public class MediaVolume {
+
+	/** Value from 0 to 1 that represents the current stream volume level */
+	@Nullable
+	@JsonProperty
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	protected final Double level;
+
+	/** Whether the stream is muted */
+	@Nullable
+	@JsonProperty
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	protected final Boolean muted;
+
+	/**
+	 * Creates a new instance using the specified parameters.
+	 *
+	 * @param level the value between 0 and 1 that represents the current stream
+	 *            volume level.
+	 * @param muted whether the stream is muted.
+	 */
+	public MediaVolume(
+		@JsonProperty("level") Double level,
+		@JsonProperty("muted") Boolean muted
+	) {
+		this.level = level;
+		this.muted = muted;
+	}
+
+	/**
+	 * @return The value between 0 and 1 that represents the current stream
+	 *         volume level.
+	 */
+	@Nullable
+	public Double getLevel() {
+		return level;
+	}
+
+	/**
+	 * @return Whether the stream is muted.
+	 */
+	@Nullable
+	public Boolean getMuted() {
+		return muted;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(level, muted);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof MediaVolume)) {
+			return false;
+		}
+		MediaVolume other = (MediaVolume) obj;
+		return Objects.equals(level, other.level) && Objects.equals(muted, other.muted);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(getClass().getSimpleName()).append(" [");
+		if (level != null) {
+			builder.append("level=").append(level).append(", ");
+		}
+		if (muted != null) {
+			builder.append("muted=").append(muted);
+		}
+		builder.append("]");
+		return builder.toString();
+	}
+}
